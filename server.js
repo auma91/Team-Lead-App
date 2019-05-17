36406e1b59a46901439f4668ce86e3a2ea7c8883
auma91 <auma9171@colorado.edu>
2019-05-17 15:19:43 -0600

    adding nodejs

---
+++ server.js
@@ -0,0 +1,24 @@
+var express = require('express'); //Ensure our express framework has been added
+var app = express();
+var bodyParser = require('body-parser');
+var session = require("express-session"); //expressSession
+app.use(bodyParser.json());              // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+app.use(session({
+  secret: 'secrettexthere',
+  saveUninitialized: true,
+  resave: true,
+}));
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/'));//This line is necessary for us to use relative paths and access our resources directory
+
+var port = process.env.PORT || 8080;
+app.listen(port, function() {
+    console.log('Our app is running on http://localhost:' + port);
+});
+
+console.log('8080 is the magic port');
+
+app.get('/', function(req, res) {
+  res.render('./views/pages/home', {});
+});
